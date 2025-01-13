111a6dbaeb681d54d8bfc5ad3498532749c18e9e
Nabeedjamshed <nabeedjamshedali800@gmail.com>
2025-01-13 22:15:33 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+alert("Welcome to this website are you happy?")
+// server and client ke bheech mai jo communication hai wo JS ki wja se hota hai without page ko reload kie
+// buyer request on fiverr and upwork
